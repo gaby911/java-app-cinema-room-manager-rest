58143ebb00aa6c441a067faff207ec2e73d59bdb
Florin <florin.abagiu91@gmail.com>
2023-05-12 07:15:34 +0300

    Improve your vehicle exercise

---
+++ Topics/Inner classes/Improve your vehicle/src/Main.java
@@ -0,0 +1,41 @@
+class Vehicle {
+
+    private String name;
+
+    // create constructor
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    class Engine {
+
+        // add field horsePower
+        int horsePower;
+
+        // create constructor
+        public Engine(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+        void start() {
+            System.out.println("RRRrrrrrrr....");
+        }
+
+        // create method printHorsePower()
+        void printHorsePower() {
+            System.out.println("Vehicle " + Vehicle.this.name + " has " + this.horsePower + " horsepower.");
+        }
+    }
+}
+
+// this code should work
+class EnjoyVehicle {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle("Dixi");
+        Vehicle.Engine engine = vehicle.new Engine(20);
+        engine.printHorsePower();
+    }
+}

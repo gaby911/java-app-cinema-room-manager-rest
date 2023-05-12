fed50738f86548cb15c306cdfe99ed723c3d31b2
Florin <florin.abagiu91@gmail.com>
2023-05-12 07:22:18 +0300

    New Inner class exercise

---
+++ Topics/Inner classes/New Inner class/src/Main.java
@@ -0,0 +1,42 @@
+class Vehicle {
+
+    private String name;
+
+    // create constructor
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    class Engine {
+
+        void start() {
+            System.out.println("RRRrrrrrrr....");
+        }
+
+    }
+
+    // create class Body
+    class Body {
+        String color;
+
+        public Body(String color) {
+            this.color = color;
+        }
+
+        void printColor() {
+            System.out.println("Vehicle " + Vehicle.this.name + " has " + this.color + " body.");
+        }
+    }
+}
+
+// this code should work
+class EnjoyVehicle {
+
+    public static void main(String[] args) {
+
+        Vehicle vehicle = new Vehicle("Dixi");
+        Vehicle.Body body = vehicle.new Body("red");
+        body.printColor();
+    }
+}

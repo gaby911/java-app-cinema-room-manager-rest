edd09e5caa24164b3b2c14c8be7ac01e4ee2a985
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2023-12-10 21:22:35 +0200

    initialized an instance of Holder

---
+++ Topics (1)/Generics and Object/Initialize an object of a generic class/src/Main.java
@@ -0,0 +1,27 @@
+/*nitialize an object of the generic class Holder to make the code in the main method compile.
+
+The program should print This is an instance of String.*/
+class Holder<T> {
+    private T value;
+
+    public void set(T value) {
+        this.value = value;
+    }
+
+    public T get() {
+        return value;
+    }
+}
+
+class Main {
+    public static void main(String... args) {
+        // initialize an instance of Holder to make the code below compiled
+        Holder<String> holder = new Holder<>();
+
+        // do not change the code below
+        holder.set("This is an instance of String");
+
+        String value = holder.get();
+        System.out.println(value);
+    }
+}

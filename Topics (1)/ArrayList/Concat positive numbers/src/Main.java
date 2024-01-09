de96fdc2cd6180cdc0f91924b27d84ec88a23f23
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2024-01-09 14:49:28 +0200

    concat 2 positive lists of numbers

---
+++ Topics (1)/ArrayList/Concat positive numbers/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+class ConcatPositiveNumbersProblem {
+
+    public static ArrayList<Integer> concatPositiveNumbers(ArrayList<Integer> l1, ArrayList<Integer> l2) {
+
+        ArrayList<Integer> finalList = new ArrayList<>();
+
+        l1.removeIf(el -> el < 0);
+        l2.removeIf(el -> el < 0);
+
+        finalList.addAll(l1);
+        finalList.addAll(l2);
+        return finalList;
+    }
+
+    /* Do not modify this method */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayList<Integer> list1 = readArrayList(scanner);
+        ArrayList<Integer> list2 = readArrayList(scanner);
+
+        ArrayList<Integer> result = concatPositiveNumbers(list1, list2);
+
+        result.forEach(n -> System.out.print(n + " "));
+    }
+
+    /* Do not modify this method */
+    private static ArrayList<Integer> readArrayList(Scanner scanner) {
+        return Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+}

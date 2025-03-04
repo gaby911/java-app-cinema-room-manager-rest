cf178ddf57cbd90efaaca9e46daf9197d6b621bf
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2025-03-04 12:35:33 +0200

    evaluate an expression requirements

--- Topics (1)/Floating-point types/Evaluate an expression/src/Main.java
+++ Topics (1)/Floating-point types/Evaluate an expression/src/Main.java
@@ -1,5 +1,19 @@
 import java.util.Scanner;
+/*Write a program that reads four double values a, b, c, d and then evaluates the following expression
 
+a * 10.5 + b * 4.4 + (c + d) / 2.2
+The program should output the result.
+
+
+Sample Input:
+1
+2.5
+0
+4.4
+
+Sample Output:
+23.5
+*/
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

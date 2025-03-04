902017a9c8b4a86dafea1522db2067db6f7097dd
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2025-03-04 10:54:55 +0200

    celsius to fahrenheit

--- Topics (1)/Floating-point types/Distance/src/Main.java
+++ Topics (1)/Floating-point types/Distance/src/Main.java
@@ -1,22 +1,12 @@
 import java.util.Scanner;
-/*Write a program that reads the distance between two cities in miles and the travel time by bus in hours and outputs the average speed of the bus.
 
-Note: there is NO need to give any explanations during input and output.
-
-
-Sample Input:
-100
-2
-
-Sample Output:
-50.0*/
 class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int cityDistance = scanner.nextInt();
         int busTime = scanner.nextInt();
 
-        int averageSpeedOfTheBus = cityDistance / busTime;
-        System.out.println(averageSpeedOfTheBus);
+        int i = cityDistance / busTime;
+        System.out.println(i);
     }
 }

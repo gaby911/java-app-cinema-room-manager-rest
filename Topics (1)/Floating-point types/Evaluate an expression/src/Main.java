4df34e3189c74fd5083e2cb26f0f3b6d86fbe7a9
Florin Abagiu <35663669+gaby911@users.noreply.github.com>
2025-03-28 06:07:49 +0200

    see as binary debugger

--- Topics (1)/Floating-point types/Evaluate an expression/src/Main.java
+++ Topics (1)/Floating-point types/Evaluate an expression/src/Main.java
@@ -1,21 +1,10 @@
 class Main {
     public static void main(String[] args) {
-        int greatestPrimeInRange = 2;
-        for (int number = 2; number <= 1000; number++) {
-            if (isPrime(number) && number > greatestPrimeInRange) {
-                greatestPrimeInRange = number;
-            }
-        }
-    }
+        int number1 = 38;
+        int number2 = 54;
 
-    static boolean isPrime(int number) {
-        for (int i = 2; i <= number / 2; i++) {
-            if (number % i != 0) {
-                continue;
-            } else {
-                return false;
-            }
-        }
-        return true;
+        number1 = number1 << 2;
+        number2 = number2 >> 1;
+        int result = number2 ^ number1;
     }
 }
